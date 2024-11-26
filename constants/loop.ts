148b39014d2c78635d60189bc78d9a0f1ff02448
borteye <gabrielborteye12199@gmail.com>
2024-11-26 01:32:06 +0000

    add: constants and loops Constant files

---
+++ constants/loop.ts
@@ -0,0 +1,46 @@
+import Images from "./Images";
+
+export const onBoarding = [
+    {
+      id: 1,
+      image: Images.onBoarding1,
+      title: "Welcome to Elextra",
+      description:
+        "Your one-stop destination for a luxurious and seamless shopping experience.",
+    },
+    {
+      id: 2,
+      image: Images.onBoarding2,
+      title: "Shop with Joy",
+      description: "Discover a world of premium products curated just for you.",
+    },
+    {
+      id: 3,
+      image: Images.onBoarding3,
+      title: "Find What You Need",
+      description:
+        "Explore categories and search effortlessly to discover your favorites.",
+    },
+    {
+      id: 4,
+      image: Images.onBoarding4,
+      title: "Shop Securely",
+      description:
+        "Your privacy and transactions are protected with top-notch security.",
+    },
+    {
+      id: 5,
+      image: Images.onBoarding5,
+      title: "Hassle-Free Payments",
+      description:
+        "Enjoy multiple secure and convenient payment options tailored for you.",
+    },
+    {
+      id: 6,
+      image: Images.onBoarding6,
+      title: "Exclusive Deals Await",
+      description:
+        "Grab amazing discounts and flash sales, available for a limited time.",
+    },
+  ];
+  
