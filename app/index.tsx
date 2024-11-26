08928a2b90c9f7be0b420743080ea42f5c806ce8
borteye <gabrielborteye12199@gmail.com>
2024-11-26 23:03:18 +0000

    add: root index file for conditionally rendering pages

---
+++ app/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import { getIsFirstLaunch } from "@/redux/features/firstLaunch";
+import { Redirect } from "expo-router";
+import OnBoarding from "./onBoarding";
+
+export default function index() {
+  const isFirstLaunch = useSelector(getIsFirstLaunch);
+
+  if (isFirstLaunch) return <OnBoarding />;
+
+  return <Redirect href="(auth)" />;
+}
