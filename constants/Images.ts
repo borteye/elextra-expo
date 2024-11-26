2c380ddc4c4583f138a85ccef29f94a29f06dd54
borteye <gabrielborteye12199@gmail.com>
2024-11-26 01:28:47 +0000

    update: add onboarding images

--- constants/Images.ts
+++ constants/Images.ts
@@ -1,3 +1,9 @@
 export default {
   logo: require("@/assets/Images/e-app-logo-raw.png"),
+  onBoarding1: require("@/assets/Images/onBoarding1.png"),
+  onBoarding2: require("@/assets/Images/onBoarding2.png"),
+  onBoarding3: require("@/assets/Images/onBoarding3.png"),
+  onBoarding4: require("@/assets/Images/onBoarding4.png"),
+  onBoarding5: require("@/assets/Images/onBoarding5.png"),
+  onBoarding6: require("@/assets/Images/onBoarding6.png"),
 };
