529b3c836f45a4e4c39d48f8134bd90fa71bcb7f
borteye <gabrielborteye12199@gmail.com>
2024-11-26 22:52:39 +0000

    add: brandHander component

---
+++ components/brandHeader.tsx
@@ -0,0 +1,30 @@
+import { View, Text, StyleSheet, Image } from "react-native";
+import React from "react";
+import Images from "@/constants/Images";
+
+export default function BrandHeader() {
+  return (
+    <View style={styles.container}>
+      <Image source={Images.logo} style={styles.image} />
+      <Text style={styles.text}>Elextra</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 15,
+  },
+  image: {
+    width: 45,
+    height: 45,
+  },
+  text: {
+    fontSize: 25,
+    color: "#DE8946",
+    fontFamily: "Poppins",
+  },
+});
